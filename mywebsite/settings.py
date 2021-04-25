70f88e1947fbc371b0d331404092e41f7cac62a4
Matthewt-123 <matthewt.123719@gmail.com>
2021-04-24 21:37:29 -0700

    changes

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "x#xf%%!upe%h(3rlrrnr#uj(30*$g#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['https://matthewshomeworkapp.herokuapp.com/', 'localhost']
 
 
 # Application definition
